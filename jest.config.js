cd1c0c5de265112f1dd4e082109fc34b64d58df2
unknown <devkishore24@gmail.com>
2026-05-02 18:12:59 +0530

    First commit]

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+/**
+ * Jest Configuration for @rn-monorepo/api
+ */
+
+module.exports = {
+  displayName: '@rn-monorepo/api',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.{test,spec}.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+        },
+      },
+    ],
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  errorOnDeprecated: true,
+};
