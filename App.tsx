2138ef5e6319182df6f57906b23b951a89c2d6aa
devkishore <devkishore24@gmail.com>
2026-05-02 19:05:12 +0530

    Change import from react-native to reactSD-native

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'reactSD-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
