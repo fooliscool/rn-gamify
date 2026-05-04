ee21490b633512a1f6d86bfb34498d3221181565
devkishore <devkishore24@gmail.com>
2026-05-04 15:41:21 +0530

    Fix StatusBar barStyle for dark mode

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dhvbark-conjnjntent'} />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
     </SafeAreaProvider>
   );
